ed65f47ce83c0a8be2edb7bc937323e00d8170ca
WinterCricket <hodhodhod2003@gmail.com>
2021-02-16 00:36:46 -0600

    functions

--- index.php
+++ index.php
@@ -1,44 +1,16 @@
 
 <?php 
 
-// $myVar1 = 'Hello';
-// $myVar2 = 'Worldly Dane';
-// $space = ' ';
-// echo $myVar1.$space.$myVar2;
+	// function greet($greeting, $name){
+	// 	echo strtoupper($greeting).' '.$name.'!';
+	// }
+	// greet('Let us roll ','Harlow Whore');
 
- $numbers = array(12, 88, 2, 34, 16);
-
-// print_r($numbers[3]);
-
-// echo $numbers[3];
-
-//associated array
-
-$ages = array(
-
-	"John" => 48,
-	"Horace"=> 2908,
-	"Robin" => 9
-);
-
-// //echo $ages['Horace'];
-
-// echo count($ages);
-
- // $i = 1;
- // while($i <=10){
- // 	echo "This little piggy number ".$i." went to market. <br/>";
- // 	$i++;
- // }
-
- //foreach loop is for arrays
-
- foreach($numbers as $number){
- 	echo 'This is number '.$number.' for the party. <br>';
- }
-
- foreach($ages as $name => $age){
- 	echo 'This is '.$name.', who is '. $age. ' years old.<br>';
- }
+$num = 10;
+if($num != 10){
+	echo "Yup!";
+} else {
+	echo "Nope!";
+}
 
  ?>
