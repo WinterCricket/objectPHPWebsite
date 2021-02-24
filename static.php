b0b3fc22db8e1056ab7109cac2c70b497e9d5806
WinterCricket <hodhodhod2003@gmail.com>
2021-02-24 00:08:01 -0600

    try some static methods and properties

--- static.php
+++ static.php
@@ -1,5 +1,23 @@
 <?php  
-
+//it's not the class that is static but the methods and properties
 class User{
-	
+	public static $minPassLength = 5;
+
+	public static function validatePassword($password){
+		if(strlen($password) >= self::$minPassLength){
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
+
+$password ='passY';
+
+if(User::validatePassword($password)){
+	echo 'Password is valid';
+} else {
+	echo 'Piss off, clown!';
+}
+
+
