a55886de148db9f91e828dd5143ce7a05a3ee960
WinterCricket <hodhodhod2003@gmail.com>
2021-02-27 01:15:37 -0600

    from abstract to class extends to new instance of

--- abstract.php
+++ abstract.php
@@ -10,4 +10,26 @@ abstract class Animal{
 	abstract public function makeSound();
 }
 
- 
+class Duck extends Animal{
+	public function describe(){
+		return parent::describe();
+	}
+	public function makeSound(){
+		return ' Quack!';
+	}
+}
+
+class Giraffe extends Animal{
+	public function describe(){
+		return parent::describe();
+	}
+	public function makeSound(){
+		return ' Geergeer!';
+	}
+}
+ //call
+$animal = new Duck();
+$animal->name = 'Mallard May';
+$animal->color = 'Green and Black';
+echo $animal->describe();
+echo $animal->makeSound();
