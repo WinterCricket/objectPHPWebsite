633b3f4dc0848d499ba9dd598ad81621b11ba6d2
WinterCricket <hodhodhod2003@gmail.com>
2021-03-04 23:43:45 -0600

    add bind function

---
+++ classes/Database.php
@@ -0,0 +1,58 @@
+<?php  
+
+
+	 class Database{
+		private	$host	= 	'localhost';
+		private	$user	= 	'root';
+	 	private	$pass	=	'root';
+		private $dbname = 	'mydb';
+
+
+		private $dbh;
+		private $error;
+		private $stmt;
+
+		//constructor
+		public function __construct(){
+			//set DSN
+			$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+		
+		// 	//set options
+			$options = array(
+				PDO::ATTR_PERSISTENT		=> 	true,
+				PDO::ATTR_ERRMODE			=> PDO::ERRMODE_EXCEPTION
+			);
+			//create new PDO instance
+			try {
+				$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+			} catch(PDOException $e){
+				$this->error = $e->getMessage();
+			}
+		}
+
+		public funciton query($query){
+			$this->stmt = $this->dbh->prepare($query);
+		}
+		public function bind($param, $value, $type = null){
+			if(is_null($type)){
+				switch(true){
+					case is_int($value):
+						$type = PDO::PARAM_INT;
+						break;
+					case is_bool($value):
+						$type = PDO::PARAM_BOOL;
+						break;
+					case is_null($value):
+						$type = PDO::PARAM_NULL;
+						break;
+						default:
+						$type = PDO::PARAM_STR;
+
+				}
+
+			}
+			$this->stmt->bindValue($param, $value, $type);
+		}
+	}
+
+?>	
